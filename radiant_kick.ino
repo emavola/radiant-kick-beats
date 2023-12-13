7998a9fd5d40672fd98d52af008c144ab1fcc438
Emanuele Volanti <emavolant@gmail.com>
2023-12-13 20:34:15 +0100

    Add buffer to trigger menu

--- radiant_kick.ino
+++ radiant_kick.ino
@@ -13,7 +13,7 @@ void setup() {
   Serial.begin(9600);
   strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
   strip.show();            // Turn OFF all pixels ASAP
-  strip.setBrightness(255); // Set BRIGHTNESS to about 1/5 (max = 255)
+  strip.setBrightness(255);
 }
 
 
@@ -22,18 +22,32 @@ int res = 0;
 int blue = 0;
 float red = 255;
 int green = 255;
-int oldAnalog = 0;
-int TRESHOLD = 6;
+int TRESHOLD = 500;
+
+bool isMenu = false;
+
+const int BUFFER_SIZE = 5;
+int audioBuffer[BUFFER_SIZE];
+int bufferIndex = -1;
+int prevKick = 0;
+
 void loop() {
   red = random(0, 255);
   blue = random(0, 255);
   green = random(0, 255);
-  if (abs(analogRead(A0) - oldAnalog) > TRESHOLD) {
-    trigger(30, 100, 100);
+
+  int a = analogRead(A0);
+  
+  //Serial.println(audioBuffer);
+  if (a > TRESHOLD) {
+    int currKick = millis();
+    Serial.println(bufferIndex);
+    audioBuffer[(bufferIndex + 1) % BUFFER_SIZE] = currKick - prevKick;
+    bufferIndex = (bufferIndex + 1) % BUFFER_SIZE;
+    prevKick = currKick;
+    printArray(audioBuffer, BUFFER_SIZE);
+    trigger(10, 10, 10);
   }
-  Serial.println(abs(analogRead(A0) - oldAnalog));
-  oldAnalog = analogRead(A0);
-  delay(10);
 }
 
 void trigger(int durationFadeIn, int durationFadeOut, int sustain) {
@@ -60,4 +74,15 @@ void fadeOut(int duration) {
     strip.show();
     delay(del);
   }
+}
+
+void printArray(int array[], int size) {
+  Serial.print("Array: ");
+  for (int i = 0; i < size; i++) {
+    Serial.print(array[i]);
+    if (i < size - 1) {
+      Serial.print(", ");
+    }
+  }
+  Serial.println();
 }
